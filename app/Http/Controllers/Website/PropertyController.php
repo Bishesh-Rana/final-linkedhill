ae8cb1a38f4bb23f45b35a551c6cf1f2f0df6baa
Bishesh-Rana <bishesh.rana@nectardigit.com>
2022-11-21 18:13:12 +0545

    final

--- app/Http/Controllers/Website/PropertyController.php
+++ app/Http/Controllers/Website/PropertyController.php
@@ -81,6 +81,10 @@ class PropertyController extends Controller
             }                        
             });
         })
+        ->when(request('area'),function($query,$var){
+            $unit = request('unit');
+            $query->where('total_area','>=',$var)->where('total_area_unit','=',$unit)
+        })
         ->when(request('listedby'), function($query,$var){
             switch($var){
                 case "owner":
