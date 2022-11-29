e0aea7d39e5e9efabd4e7dc7801883bd40fe407f
sagar <yam@nectardigit.com>
2022-11-29 13:58:14 +0545

    change design

--- resources/views/website/index/_search.blade.php
+++ resources/views/website/index/_search.blade.php
@@ -1,175 +1,169 @@
 <div class="linked_hill_search_wrapper">
-    
     <div class="container">
         <form class="searchProperty" method="get" action="{{ route('front.search-properties') }}">
-        <div class="row">
-          
-            <div class="col-lg-12">
-                <div class="linked_hill_flex_wrap">
-                    <div class="linked_hill_search_top ">
-                        <ul>
-                            @foreach ($purposes as $key => $type)
-                            <li >
-                                <input type='radio' data-element="adpurpose{{$key}}" name="purpose" value="{{ $type->name }}" id="purpose{{$key}}"/>
-                                <label for="purpose{{$key}}">{{ $type->name }}</label>
+            <div class="linked_hill_flex_wrap">
+                <div class="linked_hill_search_top ">
+                    <ul>
+                        @foreach ($purposes as $key => $type)
+                            <li>
+                                <input type='radio' data-element="adpurpose{{ $key }}" name="purpose"
+                                    value="{{ $type->name }}" id="purpose{{ $key }}" />
+                                <label for="purpose{{ $key }}">{{ $type->name }}</label>
                             </li>
-                             @endforeach
-                           
-                        </ul>
-                    </div>
-                    <div class="linked_hill_search_area d-flex">
-                        <?php $name='';
-                        ?>
-                       <div class="multiple_select2option">
-                        <select class="js-example-basic-multiple" name="property_address[]" multiple="multiple" value="{{($type->property_address) }}">
-                            @foreach ($property as $type)
-                            @if($name==$type->property_address)
-                            continue;
-                        @else
-                            <option>{{($type->property_address) }}</option>
-                            <?php
-                            $name=$type->property_address;
-                        ?>
-                        @endif
-                    @endforeach
-                          </select>
-                       </div>
-                       <button class="btn btn-dark" type="submit"><i class="las la-search"></i></button>
-                    </div>
-                </div>
-                <div class="for_toggle_view">
-                    <div class="link_hill_flex_bottom_wrapper">
-                        <div class="option_a1 property_item_linkedhill">
-                            <div class="option_1 multi_select_dropdown">
-                            <p>Property Type<i class="las la-angle-down"></i></p>
-                                <div class="option_listing_dropDown child_dropdown">
-                                    @foreach ($propertyCat as $propertyC)
-                                        <div class="list_group_category"> 
-                                            <input class="form-check-input front-category category" id="category" data-element="#advance{{ $propertyC->id }}" type="checkbox" name="category_id" value="{{ $propertyC->id }}">
-                                            <label class="form-check-label" for="initial{{ $propertyC->id }}">{{ $propertyC->name }}</label>
-                                        </div>
-                                    @endforeach 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="replace d-flex">
-                                @php $i=1 @endphp
-                                @foreach($feature_values as $key=>$values)
-                                @if($i<=3)
-                                <div class="option_a1">
-                                <select name="properties[{{$key}}]" id="">
-                                    @php $name = App\Models\Feature::where('id',$key)->value('title'); @endphp
-                                    <option selected disabled> {{$name}}</option>
-                                    <option value="any">Any</option>
-                                    @foreach($values as $value)
-                                        <option value="{{$value}}">{{$value}}+</option>
-                                    @endforeach
-                                    @php $i = $i + 1; @endphp
-                                </select>
-                                </div>
-                                @endif
-                                @endforeach                               
-                        </div>
-                        <div class="option_a1">
-                            <select name="start_prize" id="start_prize">
-                                <option selected disabled>Min Price</option>
-                                <option value="">Any</option>
-                                <option value="5000.00">Rs. 5000.00</option>
-                                <option value="10000.00">Rs. 10000.00</option>
-                                <option value="50000.00">Rs. 50000.00</option>
-                                <option value="100000.00">Rs. 100000.00</option>
-                                <option value="1000000.00">Rs. 1000000.00</option>
-                            </select>
-                        </div>
-                        <div class="option_a1">
-                            <select name="end_prize" id="end_prize">
-                                <option selected disabled>Max Price</option>
-                                <option value="">Any</option>
-                                <option value="1100000.00">Rs. 1100000.00</option>
-                                <option value="1500000.00">Rs. 1500000.00</option>
-                                <option value="2000000.00">Rs. 2000000.00</option>
-                                <option value="5000000.00">Rs. 5000000.00</option>
-                            </select>
-                        </div>
-                        {{-- @dd($units); --}}
-                        <div class="option_a1 landarea" style="display: none;">
-                            <input type="number" name="area" id="area" placeholder="area">
-                            <select class="areaunit" name="unit" id="areaunit">
-                                <option selected value=''>Unit</option>
-                                @foreach ($units as $unit)
-                                    <option value="{{$unit->id}}">{{$unit->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
+                        @endforeach
 
-                        <?php
-                            $name='';
-                        ?>
-                       
-                        <div class="option_a1  advance-search">
-                            <p data-bs-toggle="modal" data-bs-target="#advanceSearch">Advance Search</p>
-                        </div>
-                        
-                        <div class="surround_search">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    Surrounding
-                                </label>
+                    </ul>
+                </div>
+                <div class="linked_hill_search_area d-flex">
+                    <?php $name = '';
+                    ?>
+                    <div class="multiple_select2option">
+                        <select class="js-example-basic-multiple" name="property_address[]" multiple="multiple"
+                            value="{{ $type->property_address }}">
+                            @foreach ($property as $type)
+                                @if ($name == $type->property_address)
+                                    continue;
+                                @else
+                                    <option>{{ $type->property_address }}</option>
+                                    <?php
+                                    $name = $type->property_address;
+                                    ?>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
+                    <button class="btn btn-dark" type="submit"><i class="las la-search"></i></button>
+                </div>
+            </div>
+            <div class="for_toggle_view">
+                <div class="link_hill_flex_bottom_wrapper">
+                    <div class="option_a1 property_item_linkedhill">
+                        <div class="option_1 multi_select_dropdown">
+                            <p>Property Type<i class="las la-angle-down"></i></p>
+                            <div class="option_listing_dropDown child_dropdown">
+                                @foreach ($propertyCat as $propertyC)
+                                    <div class="list_group_category">
+                                        <input class="form-check-input front-category category" id="category"
+                                            data-element="#advance{{ $propertyC->id }}" type="checkbox"
+                                            name="category_id" value="{{ $propertyC->id }}">
+                                        <label class="form-check-label"
+                                            for="initial{{ $propertyC->id }}">{{ $propertyC->name }}</label>
+                                    </div>
+                                @endforeach
                             </div>
                         </div>
                     </div>
+                    <div class="replace d-flex">
+                        @php $i=1 @endphp
+                        @foreach ($feature_values as $key => $values)
+                            @if ($i <= 3)
+                                <div class="option_a1">
+                                    <select name="properties[{{ $key }}]" id="">
+                                        @php $name = App\Models\Feature::where('id',$key)->value('title'); @endphp
+                                        <option selected disabled> {{ $name }}</option>
+                                        <option value="any">Any</option>
+                                        @foreach ($values as $value)
+                                            <option value="{{ $value }}">{{ $value }}+</option>
+                                        @endforeach
+                                        @php $i = $i + 1; @endphp
+                                    </select>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="option_a1">
+                        <select name="start_prize" id="start_prize">
+                            <option selected disabled>Min Price</option>
+                            <option value="">Any</option>
+                            <option value="5000.00">Rs. 5000.00</option>
+                            <option value="10000.00">Rs. 10000.00</option>
+                            <option value="50000.00">Rs. 50000.00</option>
+                            <option value="100000.00">Rs. 100000.00</option>
+                            <option value="1000000.00">Rs. 1000000.00</option>
+                        </select>
+                    </div>
+                    <div class="option_a1">
+                        <select name="end_prize" id="end_prize">
+                            <option selected disabled>Max Price</option>
+                            <option value="">Any</option>
+                            <option value="1100000.00">Rs. 1100000.00</option>
+                            <option value="1500000.00">Rs. 1500000.00</option>
+                            <option value="2000000.00">Rs. 2000000.00</option>
+                            <option value="5000000.00">Rs. 5000000.00</option>
+                        </select>
+                    </div>
+                    {{-- @dd($units); --}}
+                    <div class="option_a1 landarea" style="display: none;">
+                        <input type="number" name="area" id="area" placeholder="area">
+                        <select class="areaunit" name="unit" id="areaunit">
+                            <option selected value=''>Unit</option>
+                            @foreach ($units as $unit)
+                                <option value="{{ $unit->id }}">{{ $unit->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <?php
+                    $name = '';
+                    ?>
+
+                    <div class="option_a1  advance-search">
+                        <p data-bs-toggle="modal" data-bs-target="#advanceSearch">Advance Search</p>
+                    </div>
+
+                    <div class="surround_search">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked"
+                                checked>
+                            <label class="form-check-label" for="flexCheckChecked">
+                                Surrounding
+                            </label>
+                        </div>
+                    </div>
                 </div>
-                <div class="linked_hill_button_wraper">
-                    <p class="click" id="click">More Search </p>
-                </div>
-                
             </div>
-        </div>
-    </form>
+            <div class="linked_hill_button_wraper">
+                <p class="click" id="click">More Search </p>
+            </div>
+        </form>
     </div>
 </div>
 
 @push('scripts')
-<script>
-     $(document).ready(function() {
-        var category_ids = [];
-        $('.category').on('change', function(){
-            let{
+    <script>
+        $(document).ready(function() {
+            var category_ids = [];
+            $('.category').on('change', function() {
+                let {
                     value,
                     id,
                     checked
                 } = event.target;
-                if(id=="category"){
-                    if(checked){
+                if (id == "category") {
+                    if (checked) {
                         category_ids.push(value);
-                    }
-                    else{
-                        category_ids = category_ids.filter(function(data){
+                    } else {
+                        category_ids = category_ids.filter(function(data) {
                             return data != value;
                         })
                     }
                 }
                 $.ajax({
-                    url: "{{route('filter')}}",
+                    url: "{{ route('filter') }}",
                     type: 'get',
                     data: {
                         category_ids: category_ids,
                     },
                     // dataType: 'JSON',
-                    success:function(response)
-                    {
+                    success: function(response) {
                         console.log(response);
                         $(".replace").replaceWith(response);
                     },
-                    error: function(response) {
-                    }
+                    error: function(response) {}
                 });
-               
-        });
-        
-     });
 
-    
-</script>
+            });
+
+        });
+    </script>
 @endpush
